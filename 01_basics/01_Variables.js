0e803fa2f757a257951f6166676dca51738df66a
Jawad Hussain <jawadbhutto41@gmail.com>
2024-11-06 08:19:07 +0000

    Datatypes and Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,22 @@
+// Variables of JavaScript
+
+const accountId = 62516251 // Const Variable use for One time Value , After value of variable can't change
+
+let accountEmail = "jawad@gmail.com" // Let variable most used in programming it can change the value
+
+var accountPassword = "Password123" //Var variable used in old time now this is not used because Scope Issues
+
+accountCity = "Larkana" // We can assign value without define variable type but it is not right way to write code 
+
+let accountState; // We can write variable without assign any value and it print as (Undefined)
+
+//const accountId = 123
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail , accountPassword, accountCity, accountState]); // Console Table used for show data in tabular form
+
+
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+let name ="Jawad Hussain"
+let age = 17
+let login= false
+
+
+// number => 2 to power 53
+// bigint 
+// String can use in '' codes but "" is recommened 
+// boolean = true/falsa
+// null = standalone value
+// undefined = empty 
+// symbol = unique
+
+// Object
+
+// how to find data type of value
+console.log(typeof age);
